f2a084c3357106332e7553c10e68586db58bb6c6
MatheusGuedes0 <matheusrsguedes1@gmail.com>
2024-10-30 22:50:10 -0300

    feat(main): write the logic behind layout

--- tipTime/app/src/main/java/br/edu/utfpr/tiptime/MainActivity.kt
+++ tipTime/app/src/main/java/br/edu/utfpr/tiptime/MainActivity.kt
@@ -5,16 +5,44 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.utfpr.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+private lateinit var binding : ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener{
+            calculateButtonOnClick()
         }
+    }
+
+    private fun calculateButtonOnClick() {
+        val stringInTextField: String = binding.constOfService.text.toString()
+        val const: Double = stringInTextField.toDoubleOrNull() ?: return
+
+        val selectedId = binding.tipOption.checkedRadioButtonId
+
+        val tipPercentage = when ( selectedId) {
+            R.id.option_twenty_percent -> 0.2
+            R.id.option_eighteen_percent -> 0.18
+            R.id.option_fifteen_percent -> 0.15
+            else -> 0.15
+        }
+
+        var tip = const * tipPercentage
+
+        var roundUp = binding.roundUpSwitch.isChecked
+
+        if(roundUp){
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount_s, formattedTip)
     }
 }
